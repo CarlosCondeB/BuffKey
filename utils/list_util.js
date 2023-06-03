e2c41f5218d1688ac32e84cdaf9a17b97bbe275b
Carlos Conde <carco0206@gmail.com>
2023-06-02 23:50:25 -0500

    Agregadas vistas de registro y actualización de proveedores, historial e inicio de sesión

--- utils/list_util.js
+++ utils/list_util.js
@@ -3,12 +3,14 @@ const listFilter = () =>
         if (e.target.matches(".seeker")) {
             if (e.key === "Escape") e.target.value = "";
 
-            document.querySelectorAll(".card").forEach((item) => {
+            document.querySelectorAll(".card-name").forEach((item) => {
                 item.textContent
                     .toLowerCase()
                     .includes(e.target.value.toLowerCase())
-                    ? item.classList.remove("filtro")
-                    : item.classList.add("filtro");
+                    ? item.parentElement.parentElement.classList.remove(
+                          "filtro"
+                      )
+                    : item.parentElement.parentElement.classList.add("filtro");
             });
         }
     });
